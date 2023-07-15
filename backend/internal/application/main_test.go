8f96149fe3d76145225e1b1cff6639103f228623
Alan Minusculi <alanminusculi@icloud.com>
2023-07-15 16:18:46 -0300

    test: added driver and passenger integration tests

--- backend/internal/application/main_test.go
+++ backend/internal/application/main_test.go
@@ -12,14 +12,11 @@ import (
 )
 
 func Test_CalculateRidePrice(t *testing.T) {
-	type args struct {
-	}
 	type fields struct {
 		input string
 	}
 	tests := []struct {
 		name               string
-		args               args
 		fields             fields
 		expectedResult     *dto.CalculateRidePriceResultDTO
 		expectedApiError   *domain.ApiError
@@ -81,7 +78,149 @@ func Test_CalculateRidePrice(t *testing.T) {
 			}
 
 			if !reflect.DeepEqual(response.StatusCode, tt.expectedStatusCode) {
-				t.Errorf("Expected status code %v, got %v", tt.expectedResult.Price, response.StatusCode)
+				t.Errorf("Expected status code %v, got %v", tt.expectedStatusCode, response.StatusCode)
+			}
+		})
+	}
+}
+
+func Test_CreateDriver(t *testing.T) {
+	type fields struct {
+		input string
+	}
+	tests := []struct {
+		name               string
+		fields             fields
+		expectedApiError   *domain.ApiError
+		expectedStatusCode int
+	}{
+		{
+			name: "Given a valid input when create driver then return created id",
+			fields: fields{
+				input: `{
+					"name": "Alan",
+					"email": "alan@email.com.br",
+					"cpf": "871.756.595-20",
+					"car-plate": "MKJ-6775"
+				}`,
+			},
+			expectedStatusCode: http.StatusCreated,
+		},
+		{
+			name: "Given an invalid input when create driver then return api error",
+			fields: fields{
+				input: `{
+					"name": "Alan",
+					"email": "alan@email.com.br",
+					"cpf": "834.326.160-76",
+					"car-plate": "MKJ-6775"
+				}`,
+			},
+			expectedApiError:   domain.NewUnprocessableEntityError("invalid_cpf", "Given CPF is not valid.", ""),
+			expectedStatusCode: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			response, err := http.Post("http://localhost:8080/drivers", "application/json", strings.NewReader(tt.fields.input))
+			if err != nil {
+				t.Errorf(err.Error())
+			}
+
+			defer func() {
+				_ = response.Body.Close()
+			}()
+
+			if tt.expectedApiError == nil {
+				apiResult := &dto.DriverDTO{}
+				body, _ := io.ReadAll(response.Body)
+				_ = json.Unmarshal(body, apiResult)
+
+				if apiResult.ID == "" {
+					t.Errorf("Expected result ID, got empty")
+				}
+			} else {
+				apiResult := &domain.ApiError{}
+				body, _ := io.ReadAll(response.Body)
+				_ = json.Unmarshal(body, apiResult)
+
+				if !reflect.DeepEqual(apiResult, tt.expectedApiError) {
+					t.Errorf("Expected api error %v, got %v", tt.expectedApiError, apiResult)
+				}
+			}
+
+			if !reflect.DeepEqual(response.StatusCode, tt.expectedStatusCode) {
+				t.Errorf("Expected status code %v, got %v", tt.expectedStatusCode, response.StatusCode)
+			}
+		})
+	}
+}
+
+func Test_CreatePassenger(t *testing.T) {
+	type fields struct {
+		input string
+	}
+	tests := []struct {
+		name               string
+		fields             fields
+		expectedApiError   *domain.ApiError
+		expectedStatusCode int
+	}{
+		{
+			name: "Given a valid input when create passenger then return created id",
+			fields: fields{
+				input: `{
+					"name": "Alan",
+					"email": "alan@email.com.br",
+					"cpf": "871.756.595-20"
+				}`,
+			},
+			expectedStatusCode: http.StatusCreated,
+		},
+		{
+			name: "Given an invalid input when create driver then return api error",
+			fields: fields{
+				input: `{
+					"name": "Alan",
+					"email": "alan@email.com.br",
+					"cpf": "834.326.160-76"
+				}`,
+			},
+			expectedApiError:   domain.NewUnprocessableEntityError("invalid_cpf", "Given CPF is not valid.", ""),
+			expectedStatusCode: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			response, err := http.Post("http://localhost:8080/passengers", "application/json", strings.NewReader(tt.fields.input))
+			if err != nil {
+				t.Errorf(err.Error())
+			}
+
+			defer func() {
+				_ = response.Body.Close()
+			}()
+
+			if tt.expectedApiError == nil {
+				apiResult := &dto.DriverDTO{}
+				body, _ := io.ReadAll(response.Body)
+				_ = json.Unmarshal(body, apiResult)
+
+				if apiResult.ID == "" {
+					t.Errorf("Expected result ID, got empty")
+				}
+			} else {
+				apiResult := &domain.ApiError{}
+				body, _ := io.ReadAll(response.Body)
+				_ = json.Unmarshal(body, apiResult)
+
+				if !reflect.DeepEqual(apiResult, tt.expectedApiError) {
+					t.Errorf("Expected api error %v, got %v", tt.expectedApiError, apiResult)
+				}
+			}
+
+			if !reflect.DeepEqual(response.StatusCode, tt.expectedStatusCode) {
+				t.Errorf("Expected status code %v, got %v", tt.expectedStatusCode, response.StatusCode)
 			}
 		})
 	}
